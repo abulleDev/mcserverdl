b2ce181cbf53839f9f6a1f036f83ca0026dad7ae
abulleDev <olives2739@gmail.com>
2025-06-10 15:40:40 +0900

    test: Add unit tests for fetch fabric version function

---
+++ pkg/fabric/fabric_versions_test.go
@@ -0,0 +1,44 @@
+package fabric_test
+
+import (
+	"testing"
+
+	"github.com/abulleDev/mcserverdl/pkg/fabric"
+)
+
+func TestFabricVersions(t *testing.T) {
+	var latestFirst, oldestFirst []string
+
+	t.Run("fetch latestFirst", func(t *testing.T) {
+		var err error
+		latestFirst, err = fabric.Versions(true)
+		if err != nil {
+			t.Fatalf("Versions(true) returned error: %v", err)
+		}
+		if len(latestFirst) == 0 {
+			t.Fatal("Versions(true) returned empty slice")
+		}
+	})
+
+	t.Run("fetch oldestFirst", func(t *testing.T) {
+		var err error
+		oldestFirst, err = fabric.Versions(false)
+		if err != nil {
+			t.Fatalf("Versions(false) returned error: %v", err)
+		}
+		if len(oldestFirst) == 0 {
+			t.Fatal("Versions(false) returned empty slice")
+		}
+	})
+
+	t.Run("check order", func(t *testing.T) {
+		if len(latestFirst) != len(oldestFirst) {
+			t.Fatalf("Length mismatch: latestFirst=%d, oldestFirst=%d", len(latestFirst), len(oldestFirst))
+		}
+		for i := range latestFirst {
+			if latestFirst[i] != oldestFirst[len(oldestFirst)-1-i] {
+				t.Fatalf("Order mismatch at index %d: latestFirst=%q, oldestFirst=%q", i, latestFirst[i], oldestFirst[len(oldestFirst)-1-i])
+			}
+		}
+	})
+}
