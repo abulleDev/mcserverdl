89a3ac0357ccdf2d608bdf5849447002db5d1c91
abulle <olives2739@gmail.com>
2026-01-07 15:22:42 +0900

    feat(provider): Add Download method

--- pkg/server/provider.go
+++ pkg/server/provider.go
@@ -4,4 +4,5 @@ type Provider interface {
 	GameVersions() ([]string, error)
 	ServerVersions(gameVersion string) ([]string, error)
 	DownloadURL(gameVersion, serverVersion string) (string, error)
+	Download(gameVersion, serverVersion, installDir string, onProgress func(current, total int64)) error
 }
