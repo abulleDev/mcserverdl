c83e35131e1334b812988d7d837e8c8cc049f431
abulle <olives2739@gmail.com>
2026-01-07 16:30:06 +0900

    feat: Implement Download function for vanilla, fabric, and paper packages

---
+++ pkg/fabric/download.go
@@ -0,0 +1,17 @@
+package fabric
+
+import (
+	"path/filepath"
+
+	"github.com/abulleDev/mcserverdl/internal"
+)
+
+func (p *Provider) Download(gameVersion, serverVersion, installDir string, onProgress func(current, total int64)) error {
+	url, err := p.DownloadURL(gameVersion, serverVersion)
+	if err != nil {
+		return err
+	}
+
+	serverJarPath := filepath.Join(installDir, "server.jar")
+	return internal.Download(url, serverJarPath, onProgress)
+}
