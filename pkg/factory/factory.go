82c497b6b6c7fbaed61a04f3a9a3ba026cbe3f56
abulle <olives2739@gmail.com>
2026-01-09 14:59:44 +0900

    feat(factory): Implement factory function to create server providers

---
+++ pkg/factory/factory.go
@@ -0,0 +1,29 @@
+package factory
+
+import (
+	"fmt"
+
+	"github.com/abulleDev/mcserverdl/pkg/provider"
+	"github.com/abulleDev/mcserverdl/pkg/provider/fabric"
+	"github.com/abulleDev/mcserverdl/pkg/provider/forge"
+	"github.com/abulleDev/mcserverdl/pkg/provider/neoforge"
+	"github.com/abulleDev/mcserverdl/pkg/provider/paper"
+	"github.com/abulleDev/mcserverdl/pkg/provider/vanilla"
+)
+
+func New(serverType string) (provider.Provider, error) {
+	switch serverType {
+	case "vanilla":
+		return vanilla.New(), nil
+	case "paper":
+		return paper.New(), nil
+	case "fabric":
+		return fabric.New(), nil
+	case "forge":
+		return forge.New(), nil
+	case "neoforge":
+		return neoforge.New(), nil
+	default:
+		return nil, fmt.Errorf("unknown server type '%s'", serverType)
+	}
+}
